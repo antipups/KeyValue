bda392dec2c6b2fee3d16059e22aa99d7ce7e57a
antipups <nikitakurkurin@protonmail.ch>
2020-08-03 20:22:40 +0300

    final version of prog

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from os import path
+import tkinter as tk
 from openpyxl import load_workbook
 
 
-def main():
-    while True:
-        title = input('Введіть назву файлу: ')
-        if not path.exists(title):
-            print('Введеного файлу не існує.')
-            continue
-        while True:
-            code = input(f'Вы у файлі: "{title}"; Введіть код (щоб повернутися назад введіть 0): ')
-            if code == '0':
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.btn = tk.Button(self,
+                             text="Пошук",
+                             command=self.say_hi)
+
+        self.entr1 = tk.Entry(self)
+        self.entr2 = tk.Entry(self)
+
+        self.label1 = tk.Label(self, text='Впишіть код')
+        self.label2 = tk.Label(self, text='Назва коду')
+
+        self.label1.grid(row=0, column=0)
+        self.label2.grid(row=0, column=1)
+        self.entr1.grid(row=1, column=0)
+        self.entr2.grid(row=1, column=1)
+        self.btn.grid(columnspan=2)
+
+    def say_hi(self):
+        wb = load_workbook('HTOP.xlsx', read_only=True)
+        for string in wb.active:
+            if string[0].value == self.entr1.get():
+                self.entr2.insert(0, string[1].value)
                 break
-            wb = load_workbook(title, read_only=True)
-            for string in wb.active:
-                if string[0].value == code:
-                    print(string[1].value)
-                    break
-            else:
-                print('Знайденого коду не існує.')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
